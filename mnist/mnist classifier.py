85ea85851cea4d5d8acde791b590e805e900ed41
FerGem33 <146904893+FerGem33@users.noreply.github.com>
2024-08-20 21:33:43 -0600

    mnist nn classifier

---
+++ mnist/mnist classifier.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+from network import NeuralNetwork, relu, softmax
+
+# import mnist dataset
+data = np.array(pd.read_csv('data/train.csv')) / 255
+data_labels = np.array(pd.read_csv('data/train_labels.csv'))
+test = np.array(pd.read_csv('data/test.csv')) / 255
+test_labels = np.array(pd.read_csv('data/test_labels.csv'))
+
+# create network
+nn = NeuralNetwork()
+nn.add_layer(784, 128, relu)
+nn.add_layer(128, 64, relu)
+nn.add_layer(64, 32, relu)
+nn.add_layer(32, 10, softmax)
+
+# train network
+nn.train(data, data_labels, epochs=10)
+
+# validation test
+permutation = np.random.permutation(len(test))
+nn.predict(test[permutation], test_labels[permutation])
---
+++ mnist/network.py
@@ -0,0 +1,105 @@
+from time import time
+import numpy as np
+
+
+def relu(z: np.ndarray) -> np.ndarray:
+    return np.maximum(0, z)
+
+
+def softmax(z: np.ndarray) -> np.ndarray:
+    # z = np.clip(z, -500, 500)  # Clip values to prevent extreme values
+    z_max = np.max(z, axis=1, keepdims=True)
+    z = np.exp(z - z_max)
+    return z / (z.sum() + 1e-8)
+
+
+def cross_entropy(a: np.ndarray, y: np.ndarray):
+    i = np.argmax(y[0])
+    # print(i, np.argmax(a[0]), a[0][i])
+    return - np.log(a[0][i])
+
+
+class Layer:
+    def __init__(self, n_in: int, n_out: int, activation):
+        self.n = n_out  # neurons in layer
+        self.W = np.random.randn(n_in, n_out) * np.sqrt(2 / n_in)  # weights
+        self.b = np.zeros((1, n_out))   # biases
+        self.activation = activation
+
+        self.a = np.empty((1, n_out))   # activations
+        self.z = np.empty((1, n_out))   # weighted sum
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self.z = np.dot(x, self.W) + self.b
+        self.a = self.activation(self.z)
+        return self.a
+
+
+class NeuralNetwork:
+    def __init__(self):
+        self.l = []
+        self.L = 0
+
+    def add_layer(self, n_in: int, n_out: int, activation):
+        self.l.append(Layer(n_in, n_out, activation))
+        self.L += 1
+
+    def forward(self, x: np.ndarray):
+        a = x
+        for layer in range(self.L):
+            a = self.l[layer].forward(a)
+
+    def backward(self, x: np.ndarray, y: np.ndarray, lr: float):
+        # Initialize the gradient for the last layer (output layer)
+        delta = self.l[-1].a - y  # dL/dz for the output layer
+
+        # Loop through layers in reverse order (starting from the last layer)
+        for i in reversed(range(1, self.L)):
+            # Gradient of weights and biases
+            self.l[i].W -= lr * np.dot(self.l[i - 1].a.T, delta)
+            self.l[i].b -= lr * np.sum(delta, axis=1, keepdims=True)
+
+            # Calculate the gradient for the next layer
+            delta = np.dot(delta, self.l[i].W.T) * (self.l[i - 1].z > 0)  # dL/dz for the next layer
+
+        # Update the weights and biases for the first layer
+        self.l[0].W -= lr * np.dot(x.T, delta)
+        self.l[0].b -= lr * np.sum(delta, axis=1, keepdims=True)
+
+    def train(self, X: np.ndarray, Y: np.ndarray, epochs: int, lr: float = 0.0001):
+        t = len(X)
+        for epoch in range(epochs):
+            # Shuffle dataset
+            shuffle = np.random.permutation(t)
+            x = X[shuffle]
+            y = Y[shuffle]
+
+            epoch_loss = 0
+            t1 = time()
+
+            for i in range(t):
+                x_i = np.array([x[i]])
+                y_i = np.array([y[i]])
+                self.forward(x_i)
+                self.backward(x_i, y_i, lr)
+                loss = np.array([cross_entropy(self.l[-1].a, y_i)])
+                epoch_loss += loss
+
+            print(f' epoch: {epoch}, Average loss: {epoch_loss / t}, Time: {time() - t1}s')
+
+    def predict(self, X: np.ndarray, Y: np.ndarray):
+        t = len(X)
+        loss = 0
+        predicted = 0
+        t1 = time()
+
+        for i in range(t):
+            x_i = np.array([X[i]])
+            y_i = np.array([Y[i]])
+            self.forward(x_i)
+            loss += np.array([cross_entropy(self.l[-1].a, y_i)])
+            if np.argmax(self.l[-1].a) == np.argmax(y_i):
+                predicted += 1
+
+        print(f'Average loss: {loss / t}, Predicted: {predicted}, Accuracy: {predicted / t}, Time: {time() - t1}s')
+        
