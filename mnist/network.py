fixed numerical instability

mnist/network.py
@@ -1,8 +1,9 @@
 from time import time
-from json import dump, load
 import numpy as np
 
+epsilon = 1e-8  # used to guarantee numerical stability
 
+# activation functions
 def relu(z: np.ndarray) -> np.ndarray:
     return np.maximum(0, z)
 
@@ -10,10 +11,12 @@ def relu(z: np.ndarray) -> np.ndarray:
 def softmax(z: np.ndarray) -> np.ndarray:
     z_max = np.max(z, axis=1, keepdims=True)
     z = np.exp(z - z_max)
-    return z / (z.sum() + 1e-8)
+    return z / (z.sum() + epsilon)
 
 
+# loss function
 def cross_entropy(a: np.ndarray, y: np.ndarray):
+    a = np.clip(a, epsilon, 1 - epsilon)
     i = np.argmax(y[0])
     return - np.log(a[0][i])
 
@@ -109,7 +112,7 @@ class NeuralNetwork:
                 y_i = np.array([y[i]])
                 self.forward(x_i)
                 self.backward(x_i, y_i, lr)
-                loss = np.array([cross_entropy(self.l[-1].a, y_i)])
+                loss = cross_entropy(self.l[-1].a, y_i)
                 epoch_loss += loss
 
             print(f' epoch: {epoch}, Average loss: {epoch_loss / t}, Time: {time() - t1}s')
@@ -124,7 +127,7 @@ class NeuralNetwork:
             x_i = np.array([X[i]])
             y_i = np.array([Y[i]])
             self.forward(x_i)
-            loss += np.array([cross_entropy(self.l[-1].a, y_i)])
+            loss += cross_entropy(self.l[-1].a, y_i)
             if np.argmax(self.l[-1].a) == np.argmax(y_i):
                 predicted += 1
 