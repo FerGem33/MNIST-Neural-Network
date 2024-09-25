3e251d0b8d9eb0deefbe88a65907f1d8123f86b3
FerGem33 <ferlr33@outlook.com>
2024-09-24 21:10:36 -0600

    cleaned code

--- mnist/fashion classifier.py
+++ mnist/fashion classifier.py
@@ -1,54 +1,9 @@
-import numpy as np
-import pandas as pd
-import ctypes
-from network import NeuralNetwork, relu, softmax
+from NetworkRunner import NetworkRunner
 
 class_names = [
     "T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
     "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
 ]
 
-# load datasets
-train = pd.read_csv('data/fashion/fashion-mnist_train.csv')
-test = pd.read_csv('data/fashion/fashion-mnist_test.csv')
-
-
-temp = train.iloc[:, 0].values.astype(int)
-train_labels = np.zeros(shape=(60000, 10))
-for i in range(60000):
-    train_labels[i][temp[i]] = 1
-train_data = train.iloc[:, 1:].values / 255
-
-temp = test.iloc[:, 0].values.astype(int)
-test_labels = np.zeros(shape=(60000, 10))
-for i in range(10000):
-    test_labels[i][temp[i]] = 1
-test_data = test.iloc[:, 1:].values / 255
-
-
-# Prevent system sleep
-ctypes.windll.kernel32.SetThreadExecutionState(0x80000002)
-
-# create network
-nn = NeuralNetwork()
-
-
-"""
-nn.add_layer(784, 128, relu)
-nn.add_layer(128, 10, softmax)
-"""
-
-nn.load_network('nn2')
-
-# train network
-nn.train(train_data, train_labels, 15, 0.0001)
-
-# validation test
-permutation = np.random.permutation(len(test_data))
-nn.predict(test_data[permutation], test_labels[permutation])
-
-# save weights and biases
-# nn.save_network('nn2')
-
-# Reset to allow sleep
-ctypes.windll.kernel32.SetThreadExecutionState(0x80000000)
+nr = NetworkRunner("fashion")
+nr. run()
--- mnist/mnist classifier.py
+++ mnist/mnist classifier.py
@@ -1,30 +1,4 @@
-import numpy as np
-import pandas as pd
+from mnist.NetworkRunner import NetworkRunner
 
-from network import NeuralNetwork, relu, softmax
-
-# import mnist dataset
-data = np.array(pd.read_csv('data/train.csv')) / 255
-data_labels = np.array(pd.read_csv('data/train_labels.csv'))
-test = np.array(pd.read_csv('data/test.csv')) / 255
-test_labels = np.array(pd.read_csv('data/test_labels.csv'))
-
-# create network
-nn = NeuralNetwork()
-
-"""
-nn.add_layer(784, 128, relu)
-nn.add_layer(128, 10, softmax)
-"""
-
-nn.load_network('nn1')
-
-# train network
-nn.train(data, data_labels, 10, 0.001)
-
-# validation test
-permutation = np.random.permutation(len(test))
-nn.predict(test[permutation], test_labels[permutation])
-
-# save weights and biases
-nn.save_network('nn1')
+nr = NetworkRunner("mnist")
+nr. run()
